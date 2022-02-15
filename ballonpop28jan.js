bda232f725035b96fcee8bf32692e0e27b11096e
Rahul85095 <rahulkumar85095@gmail.com>
2022-02-15 17:45:04 +0530

    FrontEnd

---
+++ ballonpop28jan.js
@@ -0,0 +1,6 @@
+const container=document.querySelector(".container")
+const numberballon=25;
+for(let i=1;i<=numberballon;i++)
+{
+    
+}
---
+++ first.js
@@ -0,0 +1,10 @@
+function xyz(){
+let x=76
+console.log(x)}
+xyz();
+var a={ 
+    c={
+         y:90
+    }
+
+}
